454974fb0b47b9536ed185d0dabf134354e716d3
andras.bognar <andras.bognar@munch.hu>
2022-11-16 13:18:51 +0100

    Add project files.

---
+++ TestProject1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class Class1 : BasePlugin, IWidgetPlugin
+    {
+        public bool HideInWidgetList => true;
+
+        public string GetWidgetViewComponentName(string widgetZone)
+        {
+            return "Saját widget";
+        }
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.AccountNavigationAfter, PublicWidgetZones.AccountNavigationBefore });
+        }
+    }
+}
